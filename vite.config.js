a8c3f0f7073db9eac9ec039eae5aa52aa92618a8
“Harsh <harshwalia449@gmail.com>
2024-08-10 21:36:29 +0530

    changes in vite.config

--- vite.config.js
+++ vite.config.js
@@ -5,22 +5,6 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),nodePolyfills()],
-  resolve: {
-    alias: {
-      buffer: 'buffer/',
-      process: 'process/',
-      crypto: 'crypto-browserify',
-      stream: 'stream-browserify',
-      util: 'util/',
-      assert: 'assert/',
-      path: 'path-browserify'
-    },
-  },
-  define: {
-    'process.env': {},
-    global: 'window',
-    Buffer: Buffer,
-    process: process,
-  },
+  plugins: [react(), nodePolyfills()],
+
 });
